4af2bedafa4f7f52e50e95dda85319fd1530b141
kmarmokas-at-577694503646 <Karollismarmokas@gmail.com>
2022-10-24 17:24:00 +0100

    Added header for token that has to be used for every endpoint that we call.

--- app/src/main/java/com/example/starlingbankchallenge/feature/transactionsroundup/fragment/TransactionsFragment.java
+++ app/src/main/java/com/example/starlingbankchallenge/feature/transactionsroundup/fragment/TransactionsFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 import com.example.starlingbankchallenge.base.BaseFragment;
 import com.example.starlingbankchallenge.databinding.FragmentTransactionsBinding;
 import com.example.starlingbankchallenge.feature.transactionsroundup.viewmodel.AccountViewModel;
-import com.example.starlingbankchallenge.model.AccountsItem;
+import com.example.starlingbankchallenge.model.account.AccountsItem;
 import com.example.starlingbankchallenge.utilities.AppPreference;
 import com.example.starlingbankchallenge.utilities.Const;
 
@@ -18,7 +18,6 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class TransactionsFragment extends BaseFragment<FragmentTransactionsBinding> {
-
     private AccountViewModel accountViewModel;
 
     @Override
@@ -34,6 +33,7 @@ public class TransactionsFragment extends BaseFragment<FragmentTransactionsBindi
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppPreference.init(getContext());
         if (getArguments() != null) {
 
         }
@@ -46,10 +46,11 @@ public class TransactionsFragment extends BaseFragment<FragmentTransactionsBindi
         observeData();
     }
 
+
     private void initView() {
         accountViewModel = new ViewModelProvider(requireActivity()).get(AccountViewModel.class);
     }
-
+    //observing live data and saving it in share preferences
     private void observeData(){
         accountViewModel.loadAccounts().observe(getViewLifecycleOwner(), accountsResponse -> {
             for (AccountsItem accountsItem : accountsResponse.getAccounts()) {
--- app/src/main/java/com/example/starlingbankchallenge/model/account/AccountsItem.java
+++ app/src/main/java/com/example/starlingbankchallenge/model/account/AccountsItem.java
@@ -1,4 +1,4 @@
-package com.example.starlingbankchallenge.model;
+package com.example.starlingbankchallenge.model.account;
 
 import android.os.Parcel;
 import android.os.Parcelable;
